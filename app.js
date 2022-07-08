a5985dc6cb681333ba310f0b8f626b0d01d775bc
Aryasuwegatama <aryasuwegatama@gmail.com>
2022-07-08 11:19:26 +0800

    final project

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ Then he  would feel (feeling)!.
 
 `use strict`;
 
+alert(`Hi! we are going to tell a silly animal tale but the story is not complete yet.\n you need to answer a few questions to complete the story.`)
 
 // start coding here 
 // user question variable input
@@ -45,10 +46,27 @@ for ( let i = numberofQuestions ; i >= 0 ; i--){
 
 
 // original story
-    let originalStory = `There once was a ${userInputs[0]} ${userInputs[1]} from ${userInputs[2]}. Nobody knew he was a ${userInputs[1]} because he had ${userInputs[3]} fur and ate ${userInputs[4]} ${userInputs[5]} each day.
-    He like to ${userInputs[6]} and sing "${userInputs[7]}"!. Whenever he was ${userInputs[8]}, he would start speaking ${userInputs[10]}. Then he  would feel ${userInputs[9]}!.`;
+    let originalStory = `<h2>There once was a ${userInputs[0]} ${userInputs[1]} from ${userInputs[2]}. Nobody knew he was a ${userInputs[1]} because he had ${userInputs[3]} fur and ate ${userInputs[4]} ${userInputs[5]} each day.
+    He like to ${userInputs[6]} and sing "${userInputs[7]}"!. Whenever he was ${userInputs[8]}, he would start speaking ${userInputs[10]}. Then he  would feel ${userInputs[9]}!.</h2>`;
     
-    console.log(originalStory);
+    document.write(originalStory);
+    // console.log(originalStory);
     
 
-alert(`All Done! Ready for an animal tale? \" but ridiculous hahahahaa\"`)
+alert(`All Done! Ready for an animal tale? \" but ridiculous hahahahaa\"`)  
+
+
+// for debug 
+/*let userInputs = [
+    `Greedy`,
+    `Dog`,
+    `Thailand`,
+    `purple`,
+    `50`,
+    `Meats`,
+    `run`,
+    `Little Mouse`,
+    `happy`,
+    `well`,
+    `Mandarin`,
+];*/
